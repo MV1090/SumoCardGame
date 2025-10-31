323e01ced3bffd3f3d1931c5972f28140f7073fd
Mark <mark.vale@torontofilmschool.ca>
2025-10-31 13:52:01 -0600

    Revert "Set up repo"
    This reverts commit 505069b000ae55ac9a56f8add24565ad70dd90fd.

--- Assets/Scripts/Card.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public class Card : ScriptableObject
-{
-    public string cardName;
-    public string cardDescription;
-    public Sprite cardImage;    
-}
--- Assets/Scripts/WrestlingCard.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "WrestlingCard", menuName = "Scriptable Objects/WrestlingCard")]
-public class WrestlingCard : Card
-{
-    
-}
